1db102ca4a8a8df8fd354534b4dcc163dbfe0cbc
hlongn2469 <78957509+hlongn2469@users.noreply.github.com>
2021-04-28 11:25:11 -0700

    Update scraper.java

--- scraper.java
+++ scraper.java
@@ -1,13 +1,13 @@
-import java.io.IOException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.StringTokenizer;
-
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 public class scraper{
+	
     public static void main(String[] args){
     	// arrayList data structure to contain each coin
     	ArrayList<token> list = new ArrayList<token>();
@@ -15,70 +15,75 @@ public class scraper{
         
         
         try {
-        	String url = "https://www.coingecko.com/en?page=1";
+        	String url = "https://coinmarketcap.com/all/views/all/";
         	Document doc = Jsoup.connect(url).get();
         	
         	for(Element row: doc.select("div.cmc-table__table-wrapper-outer tr.cmc-table-row")) {
-        		String delims = "[ ]+";
         		if((row.select(".cmc-table__cell--sort-by__name.cmc-table__cell--left.cmc-table__cell--sortable.cmc-table__cell--sticky.cmc-table__cell").text().equals(""))){
         			continue;
         		} else {
         			// get name
         			String name = row.select(".cmc-table__cell--sort-by__name.cmc-table__cell--left.cmc-table__cell--sortable.cmc-table__cell--sticky.cmc-table__cell").text();
-   
-        			String[] token = name.split(delims);
-    
-        			System.out.println(name);
-        	
         
         			// get symbol from url
-        			String price = row.select(".cmc-table__cell--sort-by__symbol.cmc-table__cell--left.cmc-table__cell--sortable.cmc-table__cell").text();
-        			System.out.println(price);
-        		
-        		
-        		
+        			String symbol = row.select(".cmc-table__cell--sort-by__symbol.cmc-table__cell--left.cmc-table__cell--sortable.cmc-table__cell").text();
+        					
         			// get market cap
-        			String day_change = row.select(".cmc-table__cell--sort-by__market-cap.cmc-table__cell--right.cmc-table__cell--sortable.cmc-table__cell").text();
-        			System.out.println(day_change);
+        			String market_cap = row.select(".cmc-table__cell--sort-by__market-cap.cmc-table__cell--right.cmc-table__cell--sortable.cmc-table__cell").text();
+        			
         		
         			// get price
-        			String week_change = row.select(".cmc-table__cell--sort-by__price.cmc-table__cell--right.cmc-table__cell--sortable.cmc-table__cell").text();
-        			System.out.println(week_change);
-        		
-        		
+        			String price = row.select(".cmc-table__cell--sort-by__price.cmc-table__cell--right.cmc-table__cell--sortable.cmc-table__cell").text();
+       	
         	
         			// get circulating supply from url
-        			String mark_cap = row.select(".cmc-table__cell--sort-by__circulating-supply.cmc-table__cell--right.cmc-table__cell--sortable.cmc-table__cell").text();
-        			System.out.println(mark_cap);
+        			String circ_supply = row.select(".cmc-table__cell--sort-by__circulating-supply.cmc-table__cell--right.cmc-table__cell--sortable.cmc-table__cell").text();
+        			
         		 
-        		
-        
         			// get volume 24h 
         			String volume_info = row.select(".cmc-table__cell--sort-by__volume-24-h.cmc-table__cell--right.cmc-table__cell--sortable.cmc-table__cell").text();
-        			String[] volume_token = volume_info.split(delims);
-        			String volume = volume_token[0];
-        			System.out.println(volume);
+        			
         	
         			// get hour change
-        			String supply_info = row.select(".cmc-table__cell--sort-by__percent-change-1-h.cmc-table__cell--right.cmc-table__cell--sortable.cmc-table__cell").text();
-        			System.out.println(supply_info);
+        			String hr_change = row.select(".cmc-table__cell--sort-by__percent-change-1-h.cmc-table__cell--right.cmc-table__cell--sortable.cmc-table__cell").text();
+        		
         			
         			// get day change
-        			String a = row.select(".cmc-table__cell--sort-by__percent-change-24-h.cmc-table__cell--right.cmc-table__cell--sortable.cmc-table__cell").text();
-        			System.out.println(a);
+        			String day_change = row.select(".cmc-table__cell--sort-by__percent-change-24-h.cmc-table__cell--right.cmc-table__cell--sortable.cmc-table__cell").text();
+        			
         			
         			// get week change
-        			String b = row.select(".cmc-table__cell--sort-by__percent-change-7-d.cmc-table__cell--right.cmc-table__cell--sortable.cmc-table__cell").text();
-        			System.out.println(b);
+        			String week_change = row.select(".cmc-table__cell--sort-by__percent-change-7-d.cmc-table__cell--right.cmc-table__cell--sortable.cmc-table__cell").text();
         			
-        			System.out.println("--------");
+        			
+        			// create new token with above info and add to the list
+        			token coin = new token(name, symbol, market_cap, price, circ_supply, volume_info, hr_change, day_change, week_change);
+        			list.add(coin);
         		}
         		
         	}
+        	
+        	printCSV(list);
+        	
+        	
         } catch(Exception excep) {
         	
         	excep.printStackTrace();
         }
        
     }
+    
+    public static void printCSV(ArrayList<token> list) {
+    	File csvFile = new File("coin.csv");
+    	try {
+			PrintWriter out = new PrintWriter(csvFile);
+			for(token coin: list) {
+				out.printf("%s, %s, %s, %s, %s, %s, %s, %s, %s\n", coin.getName(), coin.getSymbol(), coin.getMarketCap(), coin.getPrice(),
+						coin.getCircSupply(), coin.getVol(), coin.getHourChange(), coin.getDayChange(), coin.getWeekChange());
+			}
+			out.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
 }
