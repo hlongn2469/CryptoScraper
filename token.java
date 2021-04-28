743feee0d6c46cf7a60bed9911d45c59e9581bfd
hlongn2469 <78957509+hlongn2469@users.noreply.github.com>
2021-04-28 13:35:12 -0700

    Update token.java

--- token.java
+++ token.java
@@ -1,7 +1,19 @@
+// ============================================================================
+// token.java
+// Coinscraper project/ Cobalt Challenge
+// Author: Kray Nguyen
+// version 1
+// date: 4/28/2021
+// ============================================================================
 
+// ============================================================================
+// This class is mainly responsible for holding token information
+// ============================================================================
 public class token {
+	// token atrributes
 	private String token_name, symbol, market_cap, volume, circulating_supply, price, hour_change, day_change, week_change;
-
+	
+	// parameterized constructor
 	public token(String name, String symbol, String market_cap, String price, String cir_supply, String vol, 
 			String hour_change, String day_change, String week_change){
 		this.token_name = name;
@@ -15,6 +27,7 @@ public class token {
 		this.week_change = week_change;
 	}
 	
+	// getters for the attributes
 	public String getName() {
 		return token_name;
 	}
